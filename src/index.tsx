9d2615490623f735bd304c3c5123aea589fcdfe2
Adrien Brault <adrien.brault@gmail.com>
2023-11-27 17:54:16 +0100

    chore: add sample schema to website

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,69 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { convertJsonSchemaToGbnf } from "./convert";
 
+const defaultSchema = {
+  type: "object",
+  properties: {
+    simpleString: {
+      type: "string",
+    },
+    limitedLengthString: {
+      type: "string",
+      minLength: 5,
+      maxLength: 20,
+    },
+    enumString: {
+      type: "string",
+      enum: ["option1", "option2", "option3"],
+    },
+    patternString: {
+      type: "string",
+      pattern: "^[0-9] .+$",
+    },
+    nullableString: {
+      type: ["string", "null"],
+    },
+    integerField: {
+      type: "integer",
+    },
+    numberField: {
+      type: "number",
+    },
+    arrayField: {
+      type: "array",
+      minItems: 2,
+      maxItems: 5,
+      items: {
+        type: "string",
+      },
+    },
+    subObject: {
+      type: "object",
+      properties: {
+        subField1: { type: "number" },
+        subField2: { type: "boolean" },
+      },
+      required: ["subField1"],
+    },
+  },
+  required: [
+    "simpleString",
+    "limitedLengthString",
+    "enumString",
+    "patternString",
+    "integerField",
+    "numberField",
+    "arrayField",
+    "subObject",
+  ],
+};
 const App = () => {
-  const [input, setInput] = React.useState("");
-  const [output, setOutput] = React.useState("");
+  const [input, setInput] = React.useState(
+    JSON.stringify(defaultSchema, null, 2)
+  );
+  const [output, setOutput] = React.useState(
+    convertJsonSchemaToGbnf(defaultSchema)
+  );
 
   const handleConvert = () => {
     try {
